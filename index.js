90a0839ef4067e8818d2e97e628ddbf1caa559e0
mcquo011 <omcquown@gmail.com>
2023-03-05 12:08:45 -0600

    updated view roles, epmployees, and department functions

--- index.js
+++ index.js
@@ -8,19 +8,54 @@ function viewAllDepartments() {
   connection.query("SELECT * FROM department", (err, results) => {
     if (err) throw err;
     console.table(results);
+    promptUser();
   });
 }
 
 function viewAllRoles() {
- 
+  connection.query(
+    "SELECT roles.id, roles.title, department.name AS department, roles.salary FROM roles JOIN department ON roles.department_id = department.id",
+    (err, results) => {
+      if (err) throw err;
+      console.table(results);
+      promptUser();
+    }
+  );
 }
+
 
 function viewAllEmployees() {
-  
+  connection.query(
+    "SELECT employee.id, employee.first_name, employee.last_name, roles.title AS job_title, department.name AS department, roles.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee JOIN roles ON employee.role_id = roles.id JOIN department ON roles.department_id = department.id LEFT JOIN employee AS manager ON employee.manager_id = manager.id",
+    (err, results) => {
+      if (err) throw err;
+      console.table(results);
+      promptUser();
+    }
+  );
 }
 
+
 function addDepartment() {
-  
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Enter the name of the department:",
+      },
+    ])
+    .then((answer) => {
+      connection.query(
+        "INSERT INTO department SET ?",
+        { name: answer.name },
+        (err) => {
+          if (err) throw err;
+          console.log(`Department '${answer.name}' added successfully!`);
+          promptUser();
+        }
+      );
+    });
 }
 
 function addRole() {
