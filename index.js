0b252eadd7be35621da3a669d1c142b63ac638d2
mcquo011 <omcquown@gmail.com>
2023-03-06 12:46:34 -0600

    added add employee function and installed console.table

--- index.js
+++ index.js
@@ -1,13 +1,70 @@
 const inquirer = require("inquirer");
 const connection = require("./db/server");
+const consoleTable = require("console.table");
 
 
+init();
+
+// initial function at NPM start
+function init() {
+  promptUser();
+}
+// main prompt function
+function promptUser() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "action",
+        message: "What would you like to do?",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update an employee role",
+          "Exit",
+        ],
+      },
+    ])
+    .then((answer) => {
+      switch (answer.action) {
+        case "View all departments":
+          viewAllDepartments();
+          break;
+        case "View all roles":
+          viewAllRoles();
+          break;
+        case "View all employees":
+          viewAllEmployees();
+          break;
+        case "Add a department":
+          addDepartment();
+          break;
+        case "Add a role":
+          addRole();
+          break;
+        case "Add an employee":
+          addEmployee();
+          break;
+        case "Update an employee role":
+          updateEmployeeRole();
+          break;
+        case "Exit":
+          connection.end();
+          break;
+      }
+    });
+}
 
 // functions for each option
 function viewAllDepartments() {
   connection.query("SELECT * FROM department", (err, results) => {
     if (err) throw err;
-    console.table(results);
+    console.log("\n");
+    console.table((results));
     promptUser();
   });
 }
@@ -59,66 +116,110 @@ function addDepartment() {
 }
 
 function addRole() {
-  
+  connection.query("SELECT * FROM department", (err, departments) => {
+    if (err) throw err;
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "title",
+          message: "Enter the title of the role:",
+        },
+        {
+          type: "input",
+          name: "salary",
+          message: "Enter the salary of the role:",
+        },
+        {
+          type: "list",
+          name: "department",
+          message: "Select the department of the role:",
+          choices: departments.map((department) => ({
+            name: department.name,
+            value: department.id,
+          })),
+        },
+      ])
+      .then((answer) => {
+        connection.query(
+          "INSERT INTO roles SET ?",
+          {
+            title: answer.title,
+            salary: answer.salary,
+            department_id: answer.department,
+          },
+          (err) => {
+            if (err) throw err;
+            console.log(`Role '${answer.title}' added successfully!`);
+            promptUser();
+          }
+        );
+      });
+  });
 }
 
 function addEmployee() {
-  
+  connection.query("SELECT * FROM roles", (err, roles) => {
+    if (err) throw err;
+    connection.query("SELECT * FROM employee", (err, managers) => {
+      if (err) throw err;
+      console.table(roles);
+      console.table(managers);
+
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            name: "firstName",
+            message: "What is the employee's first name?",
+          },
+          {
+            type: "input",
+            name: "lastName",
+            message: "What is the employee's last name?",
+          },
+          {
+            type: "list",
+            name: "roleId",
+            message: "What is the employee's role?",
+            choices: roles.map((role) => role.title),
+          },
+          {
+            type: "list",
+            name: "managerId",
+            message: "Who is the employee's manager?",
+            choices: managers.map(
+              (manager) => `${manager.first_name} ${manager.last_name}`
+            ),
+          },
+        ])
+        .then((answer) => {
+          const role = roles.find((role) => role.title === answer.roleId);
+          const manager = managers.find(
+            (manager) =>
+              `${manager.first_name} ${manager.last_name}` === answer.managerId
+          );
+          connection.query(
+            "INSERT INTO employee SET ?",
+            {
+              first_name: answer.firstName,
+              last_name: answer.lastName,
+              role_id: role.id,
+              manager_id: manager.id,
+            },
+            (err, res) => {
+              if (err) throw err;
+              console.log(`\n${res.affectedRows} employee added!\n`);
+              promptUser();
+            }
+          );
+        });
+    });
+  });
 }
+
 
 function updateEmployeeRole() {
   
 }
 
-// main prompt function
-function promptUser() {
-  inquirer
-    .prompt([
-      {
-        type: "list",
-        name: "action",
-        message: "What would you like to do?",
-        choices: [
-          "View all departments",
-          "View all roles",
-          "View all employees",
-          "Add a department",
-          "Add a role",
-          "Add an employee",
-          "Update an employee role",
-          "Exit",
-        ],
-      },
-    ])
-    .then((answer) => {
-      switch (answer.action) {
-        case "View all departments":
-          viewAllDepartments();
-          break;
-        case "View all roles":
-          viewAllRoles();
-          break;
-        case "View all employees":
-          viewAllEmployees();
-          break;
-        case "Add a department":
-          addDepartment();
-          break;
-        case "Add a role":
-          addRole();
-          break;
-        case "Add an employee":
-          addEmployee();
-          break;
-        case "Update an employee role":
-          updateEmployeeRole();
-          break;
-        case "Exit":
-          connection.end();
-          break;
-      }
-    });
-}
-
-
-promptUser();
